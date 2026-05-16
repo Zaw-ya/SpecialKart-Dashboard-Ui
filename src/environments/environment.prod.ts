61628ee2e93fc4fcf0ce463e8b469bc58117f034
alihani2002 <alyhani2002@gmail.com>
2026-05-16 13:14:40 +0300

    feat: configure production environment and Vercel rewrite rules for API proxying

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: '/api'
+  apiUrl: 'https://specialcart-dashboard.tryasp.net/api'
 };
